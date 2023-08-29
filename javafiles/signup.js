31ac1eafcb5c8e78eb6b847783c3e4dd864d417d
MamaKbubu <maboelatk@gmail.com>
2023-08-29 22:59:30 +0200

    updates

--- javafiles/signup.js
+++ javafiles/signup.js
@@ -1,56 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('form');
-    const usernameInput = document.getElementById('username');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    const password2Input = document.getElementById('password2');
-    const mobAgentCheckbox = document.querySelector('input[name="Mob"][value="Mob Agent"]');
-
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        validateInputs();
-
-        if (mobAgentCheckbox.checked) {
-            window.location.href = 'business.html'; // Redirect to business.html
-        }
-    });
-
-    const setError = (element, message) => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
-
-        errorDisplay.textContent = message;
-        inputControl.classList.add('error');
-        inputControl.classList.remove('success');
-    };
-
-    const setSuccess = element => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
-
-        errorDisplay.textContent = '';
-        inputControl.classList.add('success');
-        inputControl.classList.remove('error');
-    };
-
-    const isValidEmail = email => {
-        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return emailRegex.test(String(email).toLowerCase());
-    };
-
-    const validateInputs = () => {
-        const usernameValue = usernameInput.value.trim();
-        const emailValue = emailInput.value.trim();
-        const passwordValue = passwordInput.value.trim();
-        const password2Value = password2Input.value.trim();
-
-        // Validation logic for username, email, password, and password2 inputs...
-
-    };
-
-    mobAgentCheckbox.addEventListener('change', event => {
-        if (mobAgentCheckbox.checked) {
-            window.location.href = 'business.html'; // Redirect to business.html
-        }
-    });
-});
+let agree = document.getElementById("agree");
+agree.addEventListener('change', function(){
+    let Register = document.getElementById("Register");
+    if (agree.checked) {
+        Register.removeAttribute("disabled");
+        window.location.href = "business.html";
+    }
+})
+let disagree = document.getElementById("disagree");
+disagree.addEventListener('change', function(){
+    let Register = document.getElementById("Register");
+    if (disagree.checked) {
+        Register.removeAttribute("disabled");
+        window.location.href = "homepage.html";
+    }
+})
